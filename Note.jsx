8a5bca5b6f8313f51df74fb3de089d56fd0898dc
aaraika <70572585+aaraika@users.noreply.github.com>
2021-06-05 20:09:15 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+  return (
+   <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p>this was an amazing bootcamp. We covered eveyting from scratch including javascript and react.js,HTML.
+    
+     </p>
+   </div>
+    );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import APP from "./components/APP"
+
+
+ReactDOM.render(
+  <div>
+  <APP />
+  </div>,
+  document.getElementById("root")
+);
